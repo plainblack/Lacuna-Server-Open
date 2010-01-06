ceccffd0c14cde5b42f7ad3fa6be49803cda0302
JT Smith <jt@lacunaexpanse.com>
2010-01-05 19:59:08 -0500

    added regexp common

--- bin/install-pm.sh
+++ bin/install-pm.sh
@@ -1,1 +1,1 @@
-cpan SimpleDB::Class JSON::RPC::Dispatcher String::Random List::Util::WeightedChoice List::Util List::MoreUtils DateTime Coro AnyEvent
+cpan SimpleDB::Class JSON::RPC::Dispatcher String::Random List::Util::WeightedChoice List::Util List::MoreUtils DateTime Coro AnyEvent Regexp::Common
