73e634b411125ed8041ae3f9636f5973b346fdcc
JT Smith <jt@lacunaexpanse.com>
2010-03-17 16:16:52 -0500

    pull the building stats out of the classes

---
+++ bin/get_building_stats.pl
@@ -0,0 +1,33 @@
+use 5.010;
+use lib '../lib';
+use Lacuna::DB;
+use Module::Find;
+
+open my $file, '>', '/tmp/stats.csv';
+print {$file} 'Name,Energy Hour,Food Hour,Ore Hour,Water Hour,Waste Hour,Happiness Hour,Energy Cost,Food Cost,Ore Cost,Water Cost,Waste Cost,Time Cost,Energy Storage,Food Storage,Ore Storage,Water Storage,Waste Storage'."\n";
+foreach my $module (findallmod Lacuna::DB::Building) {
+    my @row;
+    my $object = $module->new(simpledb=>'xxx');
+    $object->level(1);
+    next if $object->name eq 'Building';
+    push @row, $object->name;
+    push @row, $object->energy_hour;
+    push @row, $object->food_hour;
+    push @row, $object->ore_hour;
+    push @row, $object->water_production - $object->water_consumption; # depends on planet, so faking it
+    push @row, $object->waste_hour;
+    push @row, $object->happiness_hour;
+    push @row, $object->energy_to_build;
+    push @row, $object->food_to_build;
+    push @row, $object->ore_to_build;
+    push @row, $object->water_to_build;
+    push @row, $object->waste_to_build;
+    push @row, $object->time_to_build;
+    push @row, $object->energy_storage;
+    push @row, $object->food_storage;
+    push @row, $object->ore_storage;
+    push @row, $object->water_storage;
+    push @row, $object->waste_storage;
+    print {$file} join(",", @row)."\n";
+}
+close $file;
