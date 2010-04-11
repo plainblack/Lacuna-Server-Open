e4d193b66d2a23dddc400725307308e83f3ad3bb
JT Smith <jt@lacunaexpanse.com>
2010-04-11 13:14:12 -0400

    fixed waste_capacity on get stats after upgrade changes related to rolling out nginix

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
 perl generate_docs.pl > /dev/null
 memcached -d -u nobody -m 512
-plackup --env prod --server Plack::Handler::Standalone --max-workers=10 --port 80 lacuna.psgi
+plackup --env prod --server Plack::Handler::Standalone --max-workers=10 --port 5000 lacuna.psgi
