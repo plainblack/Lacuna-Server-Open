fbee6644cb9ad9371a5b049bb239c29ae3513721
JT Smith <jt@lacunaexpanse.com>
2010-04-16 16:07:59 -0500

    adding upgrades folder

---
+++ var/upgrades/insert_home_probes.pl
@@ -0,0 +1,13 @@
+use 5.010;
+use lib '/data/Lacuna-Server/lib';
+use Lacuna::DB;
+use Lacuna;
+
+my $config = Lacuna->config;
+my $db = Lacuna::DB->new( access_key => $config->get('access_key'), secret_key => $config->get('secret_key'), cache_servers => $config->get('memcached')); 
+
+my $empires = $db->domain('empire')->search;
+
+while (my $empire = $empires->next) {
+    $empire->add_probe($empire->home_planet->star_id);
+}
