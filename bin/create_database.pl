ff5c1bc342acb58f9ac626e11fdda343dcfb7289
JT Smith <jt@lacunaexpanse.com>
2010-05-07 14:23:08 -0500

    getting rid of simpledb scripts

--- bin/create_database.pl
+++
@@ -1,7 +0,0 @@
-use lib '/data/Lacuna-Server/lib';
-use strict;
-use Lacuna::DB;
-#use Lacuna;
-
-my $db = Lacuna::DB->connect('DBI:mysql:lacuna','root');
-$db->create_ddl_dir(['MySQL'],'1.0','/data/Lacuna-Server/var/');
