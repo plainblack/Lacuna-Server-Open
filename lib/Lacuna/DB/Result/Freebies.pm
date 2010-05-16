3e8db2676a426f632d4c6dda82ef241784b45bb6
JT Smith <jt@lacunaexpanse.com>
2010-05-16 14:59:07 -0500

    more tests passing

--- lib/Lacuna/DB/Result/Freebies.pm
+++ lib/Lacuna/DB/Result/Freebies.pm
@@ -10,7 +10,7 @@ __PACKAGE__->add_columns(
     body_id                 => { data_type => 'int', size => 11, is_nullable => 0 },
     class                   => { data_type => 'char', size => 255, is_nullable => 0 },
     level                   => { data_type => 'int', size => 3, is_nullable => 0 },
-    extra_build_level       => { data_type => 'int', size => 3, default => 0},
+    extra_build_level       => { data_type => 'int', size => 3, is_nullable => 1, default => 0 },
 );
 
 __PACKAGE__->belongs_to('body', 'Lacuna::DB::Result::Map::Body', 'body_id');
