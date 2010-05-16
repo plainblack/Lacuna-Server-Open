246a2f217ec514a3cedda56edeb7b54ab6f8c260
JT Smith <jt@lacunaexpanse.com>
2010-05-15 23:37:21 -0500

    removing unnecessary hierarchy

--- lib/Lacuna/DB/Result/Building/Food/Farm.pm
+++
@@ -1,7 +0,0 @@
-package Lacuna::DB::Result::Building::Food::Farm;
-
-use Moose;
-extends 'Lacuna::DB::Result::Building::Food';
-
-no Moose;
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
