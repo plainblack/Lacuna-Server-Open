6bf7d1f113d9405dc8ae6857d14ac5a803865c47
JT Smith <jt@lacunaexpanse.com>
2010-05-24 18:46:26 -0500

    hot restart

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,6 @@
 perl generate_docs.pl > /dev/null
 memcached -d -u nobody -m 512
-starman --preload-app --workers 10 --port 5000 lacuna.psgi
+#starman --preload-app --workers 10 --port 5000 lacuna.psgi
+start_server --port 5000 -- starman --workers 10 --preload-app lacuna.psgi
+nginx -c /data/Lacuna-Server/etc/nginx.conf
+
