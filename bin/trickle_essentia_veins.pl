ca0c5740c1fbfab47b53072302c46e45f5dffa88
JT Smith <jt@lacunaexpanse.com>
2010-06-22 19:19:23 -0500

    added essentia trickler

---
+++ bin/trickle_essentia_veins.pl
@@ -0,0 +1,58 @@
+use 5.010;
+use strict;
+use lib '/data/Lacuna-Server/lib';
+use Lacuna::DB;
+use Lacuna;
+use Lacuna::Util qw(format_date to_seconds);
+use Getopt::Long;
+$|=1;
+
+our $quiet;
+
+GetOptions(
+    'quiet'         => \$quiet,  
+);
+
+out('Started');
+my $start = DateTime->now;
+
+out('Loading DB');
+our $db = Lacuna->db;
+
+trickle();
+
+
+my $finish = DateTime->now;
+out('Finished');
+out((to_seconds($finish - $start)/60)." minutes have elapsed");
+
+
+###############
+## SUBROUTINES
+###############
+
+
+sub trickle {
+    my $veins = $db->resultset('Lacuna::DB::Result::Building::Permanent::EssentiaVein');
+    while (my $vein = $veins->next) {
+        my $body = $vein->body;
+        if ($body->empire_id) {
+            my $empire = $body->empire;
+            if (defined $empire) {
+                $empire->add_essentia(4, 'Essentia Vein')->update;
+            }
+        }
+    }
+}
+
+
+# UTILITIES
+
+sub out {
+    my $message = shift;
+    unless ($quiet) {
+        say format_date(DateTime->now), " ", $message;
+    }
+}
+
+
