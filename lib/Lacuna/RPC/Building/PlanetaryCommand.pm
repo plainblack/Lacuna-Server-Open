cbadd1fd696c8169c28c65031f1c99141b52990b
JT Smith <jt@lacunaexpanse.com>
2010-07-11 15:15:15 -0500

    removed "view plans"

--- lib/Lacuna/RPC/Building/PlanetaryCommand.pm
+++ lib/Lacuna/RPC/Building/PlanetaryCommand.pm
@@ -21,25 +21,6 @@ around 'view' => sub {
     return $out;
 };
 
-sub view_plans {
-    my ($self, $session_id, $building_id) = @_;
-    my $empire = $self->get_empire_by_session($session_id);
-    my $building = $self->get_building($empire, $building_id);
-    my $free_stuff = $building->body->plans;
-    my %plans;
-    while (my $plan = $free_stuff->next) {
-        my $level = $plan->level;
-        if ($plan->extra_build_level) {
-            $level .= ' ('.$plan->extra_build_level.')';
-        }
-        $plans{$plan->class->name} = $level;
-    }
-    return {
-        plans       => \%plans,
-        status      => $self->format_status($empire, $building->body),
-    };
-}
-
 __PACKAGE__->register_rpc_method_names(qw(view_plans));
 
 
