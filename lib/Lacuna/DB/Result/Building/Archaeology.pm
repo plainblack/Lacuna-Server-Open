795c79eadc2ed3224ec1177fd5419cb4889660e9
JT Smith <jt@lacunaexpanse.com>
2010-07-22 17:15:48 -0400

    hash not array of hash

--- lib/Lacuna/DB/Result/Building/Archaeology.pm
+++ lib/Lacuna/DB/Result/Building/Archaeology.pm
@@ -49,14 +49,14 @@ sub chance_of_glyph {
 sub get_ores_available_for_processing {
     my ($self) = @_;
     my $body = $self->body;
-    my @available;
+    my %available;
     foreach my $type (ORE_TYPES) {
         my $stored = $type.'_stored';
         if ($body->$stored >= 10_000) {
-            push @available, { $type => $body->$stored };
+            $available{ $type } = $body->$stored;
         }
     }
-    return \@available;
+    return \%available;
 }
 
 sub can_search_for_glyph {
