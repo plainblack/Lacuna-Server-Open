59919b2afae221e13cbcf53f95fb230cd4fd481b
JT Smith <jt@lacunaexpanse.com>
2010-08-09 13:58:49 -0500

    Bad name

--- lib/Lacuna/RPC/Stats.pm
+++ lib/Lacuna/RPC/Stats.pm
@@ -13,7 +13,7 @@ sub credits {
             { 'Game Server'         => ['JT Smith']},
             { 'Art and Icons'       => ['Ryan Knope','JT Smith','Joseph Wain / glyphish.com','Keegan Runde']},
             { 'Geology Consultant'  => ['Geofuels, LLC / geofuelsllc.com']},
-            { 'Playtesters'         => ['John Oettinger','Jamie Vrbsky','Mike Kastern','Chris Burr','Eric Patterson','Frank Dillon','Kristi McCombs','Ryan McCombs','Mike Helfman','Tavis Parker','Sarah Bownds','Rob Dicke','Gemma Blair']},
+            { 'Play Testers'         => ['John Oettinger','Jamie Vrbsky','Mike Kastern','Chris Burr','Eric Patterson','Frank Dillon','Kristi McCombs','Ryan McCombs','Mike Helfman','Tavis Parker','Sarah Bownds','Rob Dicke','Gemma Blair']},
             { 'Game Support'        => ['Plain Black Corporation / plainblack.com']},
             ];
 }
