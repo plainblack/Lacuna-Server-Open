ba06c6b2344f3fcc190319b23b558c2883cb55fc
JT Smith <jt@lacunaexpanse.com>
2010-09-08 14:56:42 -0500

    too expensive

--- lib/Lacuna/DB/Result/Ships/SecurityMinistrySeeker.pm
+++ lib/Lacuna/DB/Result/Ships/SecurityMinistrySeeker.pm
@@ -7,10 +7,10 @@ extends 'Lacuna::DB::Result::Ships';
 use constant prereq         => { class=> 'Lacuna::DB::Result::Building::MunitionsLab',  level => 20 };
 use constant base_food_cost      => 20000;
 use constant base_water_cost     => 50000;
-use constant base_energy_cost    => 300000;
-use constant base_ore_cost       => 350000;
+use constant base_energy_cost    => 150000;
+use constant base_ore_cost       => 200000;
 use constant base_time_cost      => 58500;
-use constant base_waste_cost     => 80000;
+use constant base_waste_cost     => 40000;
 use constant base_speed     => 1000;
 use constant base_stealth   => 2000;
 
