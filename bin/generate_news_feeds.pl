c7e822d0ec1eca2b1c98058aaaec54a9786df494
JT Smith <jt@lacunaexpanse.com>
2010-09-09 15:59:17 -0400

    showing last 100 stories whether they are old or not

--- bin/generate_news_feeds.pl
+++ bin/generate_news_feeds.pl
@@ -7,7 +7,6 @@ use DateTime;
 use SOAP::Amazon::S3;
 
 my $config = Lacuna->config;
-my $age = DateTime->now->subtract(hours=>24);
 my $db = Lacuna->db;
 my $s3 = SOAP::Amazon::S3->new($config->get('access_key'), $config->get('secret_key'), { RaiseError => 1 });
 my $bucket = $s3->bucket($config->get('feeds/bucket'));
@@ -23,7 +22,6 @@ foreach my $x (int($config->get('map_size/x')->[0]/250) .. int($config->get('map
         my $rs = $news_domain->search(
             {
                 zone        => $zone,
-                date_posted => [ '>=', $age ],
             },
             {
                 rows        => 100,
@@ -31,6 +29,7 @@ foreach my $x (int($config->get('map_size/x')->[0]/250) .. int($config->get('map
             }
         );
         while (my $story = $rs->next) {
+	    say $story->headline;
             my $item = $feed->add_item;
             $item->title($story->headline);
             $item->pubDate($story->date_posted);
