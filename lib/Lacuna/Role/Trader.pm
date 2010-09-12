68a5b9e10012a15088048334d8d57c0a5245a3af
JT Smith <jt@lacunaexpanse.com>
2010-09-12 16:49:47 -0400

    bad qw

--- lib/Lacuna/Role/Trader.pm
+++ lib/Lacuna/Role/Trader.pm
@@ -220,7 +220,7 @@ sub structure_push {
     my $space_used;
     foreach my $item (@{$items}) {
         given($item->{type}) {
-            when ([qw(water energy waste ORE_TYPES FOOD_TYPES)]) {
+            when ([qw(water energy waste), ORE_TYPES, FOOD_TYPES]) {
                  confess $have_exception unless ($body->type_stored($item->{type}) >= $item->{quantity});
                  $space_used += $item->{quantity};
              }
