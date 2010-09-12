07bd1cad10cbcb24f4c9aee5018c656b8017b109
JT Smith <jt@lacunaexpanse.com>
2010-09-12 16:47:44 -0400

    fixed pushing problem

--- lib/Lacuna/Role/Trader.pm
+++ lib/Lacuna/Role/Trader.pm
@@ -242,7 +242,7 @@ sub structure_push {
     my $payload;
     foreach my $item (@{$items}) {
         given($item->{type}) {
-            when ([qw(water energy waste ORE_TYPES FOOD_TYPES)]) {
+            when ([qw(water energy waste), ORE_TYPES, FOOD_TYPES]) {
                  $body->spend_type($item->{type}, $item->{quantity});
                  $body->update;
                  $payload->{resources}{$item->{type}} += $item->{quantity};
