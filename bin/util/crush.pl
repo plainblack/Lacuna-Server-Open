9e08049724ccf51f035bb64db536914e4599edb3
JT Smith <jt@lacunaexpanse.com>
2010-09-17 13:31:07 -0500

    fixed crusher path

--- bin/util/crush.pl
+++ bin/util/crush.pl
@@ -2,13 +2,15 @@ use strict;
 use File::Find;
 use 5.010;
 
-my $pngout = '/usr/bin/pngout';
+my $pngout = '/usr/local/bin/pngout';
 my $path = $ARGV[0];
 
 find(sub {
    if (-f $File::Find::name && $File::Find::name =~ m/\.png$/) {
         say $File::Find::name;
-	system($pngout . ' '. $File::Find::name);
+	    my $cmd = $pngout . ' '. $File::Find::name;
+        say $cmd;
+        say `$cmd`;
    }
 },
 $path);
