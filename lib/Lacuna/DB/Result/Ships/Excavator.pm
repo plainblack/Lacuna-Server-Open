a9fdfc1ced2b35735d0b36d16474d865c7831dc4
JT Smith <jt@lacunaexpanse.com>
2010-09-25 13:47:05 -0500

    added resource cherry to excavators

--- lib/Lacuna/DB/Result/Ships/Excavator.pm
+++ lib/Lacuna/DB/Result/Ships/Excavator.pm
@@ -4,7 +4,7 @@ use Moose;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Ships';
 use Lacuna::Util qw(randint random_element);
-use Lacuna::Constants qw(ORE_TYPES FINDABLE_PLANS);
+use Lacuna::Constants qw(ORE_TYPES FINDABLE_PLANS FOOD_TYPES);
 
 use constant prereq         => { class=> 'Lacuna::DB::Result::Building::Archaeology',  level => 15 };
 use constant base_food_cost      => 400;
@@ -15,7 +15,7 @@ use constant base_time_cost      => 20000;
 use constant base_waste_cost     => 1200;
 use constant base_speed     => 1800;
 use constant base_stealth   => 0;
-use constant base_hold_size => 80;
+use constant base_hold_size => 0;
 
 around 'build_tags' => sub {
     my ($orig, $class) = @_;
@@ -28,16 +28,16 @@ sub arrive {
     my $remote_body = $self->foreign_body;
     my $body = $self->body;
     my $empire = $body->empire;
-    if ($find < 5) {
+    if ($find < 6) {
         my $class = random_element([FINDABLE_PLANS]);
-        my $plan = $body->add_plan($class, 1, ($find < 3) ? 1 : 0);
+        my $plan = $body->add_plan($class, 1, ($find < 3) ? randint(1,3) : 0);
         $empire->send_predefined_message(
             tags        => ['Alert'],
             filename    => 'plan_discovered_by_excavator.txt',
             params      => [$remote_body->name, ($plan->level + $plan->extra_build_level), $class->name, $body->name],
         );
     }
-    elsif ($find < 15) {
+    elsif ($find < 16) {
         my $ore = random_element([ORE_TYPES]);
         $body->add_glyph($ore);
         $empire->send_predefined_message(
@@ -53,6 +53,16 @@ sub arrive {
         );
         $empire->add_medal($ore.'_glyph');
         $body->add_news(70, sprintf('%s has uncovered a rare and ancient %s glyph on %s.',$empire->name, $ore, $remote_body->name));
+    }
+    elsif ($find < 66) {
+        my $type = random_element([ORE_TYPES, FOOD_TYPES, qw(water energy)]);
+        my $amount = randint(100,2500);
+        $body->add_type($type, $amount)->update;
+        $empire->send_predefined_message(
+            tags        => ['Alert'],
+            filename    => 'resources_discovered_by_excavator.txt',
+            params      => [$remote_body->name, $amount, $type, $body->name],
+        );
     }
     else {
         $empire->send_predefined_message(
