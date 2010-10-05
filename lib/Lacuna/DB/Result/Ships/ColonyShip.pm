24e4dd465d898c71268e5477b7776ec929a653da
JT Smith <jt@lacunaexpanse.com>
2010-10-04 19:25:29 -0500

    fixed trading with colony ships, and lowered costs

--- lib/Lacuna/DB/Result/Ships/ColonyShip.pm
+++ lib/Lacuna/DB/Result/Ships/ColonyShip.pm
@@ -4,12 +4,12 @@ use Moose;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Ships';
 
-use constant prereq         => { class=> 'Lacuna::DB::Result::Building::Observatory',  level => 10 };
-use constant base_food_cost      => 100000;
-use constant base_water_cost     => 100000;
-use constant base_energy_cost    => 100000;
-use constant base_ore_cost       => 100000;
-use constant base_time_cost      => 43200;
+use constant prereq         => { class=> 'Lacuna::DB::Result::Building::Observatory',  level => 5 };
+use constant base_food_cost      => 45000;
+use constant base_water_cost     => 45000;
+use constant base_energy_cost    => 45000;
+use constant base_ore_cost       => 45000;
+use constant base_time_cost      => 60 * 60 * 24;
 use constant base_waste_cost     => 10000;
 use constant base_speed     => 455;
 use constant base_stealth   => 0;
@@ -27,12 +27,19 @@ sub arrive {
     if ($self->direction eq 'out') {
         my $planet = $self->foreign_body;
         if ($planet->is_locked || $planet->empire_id) {
-            $self->turn_around;
-            $empire->send_predefined_message(
-                tags        => ['Alert'],
-                filename    => 'cannot_colonize.txt',
-                params      => [$planet->name, $planet->name],
-            );
+            if (scalar keys %{$self->payload}) {
+                unless ($self->capture_with_spies) {
+                    $self->handle_cargo_exchange;
+                }
+            }
+            else {
+                $self->turn_around;
+                $empire->send_predefined_message(
+                    tags        => ['Alert'],
+                    filename    => 'cannot_colonize.txt',
+                    params      => [$planet->name, $planet->name],
+                );
+            }
         }
         else {
             $planet->lock;
