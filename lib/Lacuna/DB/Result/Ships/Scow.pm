593ace0665f5333d214233b279b8a55c09a9c7e6
JT Smith <jt@lacunaexpanse.com>
2010-10-05 19:42:21 -0500

    better scow

--- lib/Lacuna/DB/Result/Ships/Scow.pm
+++ lib/Lacuna/DB/Result/Ships/Scow.pm
@@ -4,17 +4,16 @@ use Moose;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Ships';
 
-use constant prereq         => { class=> 'Lacuna::DB::Result::Building::Waste::Sequestration',  level => 15 };
+use constant prereq         => { class=> 'Lacuna::DB::Result::Building::Waste::Sequestration',  level => 10 };
 use constant base_food_cost      => 2000;
 use constant base_water_cost     => 5200;
 use constant base_energy_cost    => 32400;
 use constant base_ore_cost       => 28400;
 use constant base_time_cost      => 14600;
 use constant base_waste_cost     => 8400;
-use constant base_speed     => 900;
+use constant base_speed     => 400;
 use constant base_stealth   => 100;
-use constant base_hold_size => 1000;
-
+use constant base_hold_size => 2100;
 
 around 'build_tags' => sub {
     my ($orig, $class) = @_;
