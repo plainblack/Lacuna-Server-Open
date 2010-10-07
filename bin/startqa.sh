c7d697c6502363c3bb900e4d04ed1937ad2dfd28
JT Smith <jt@lacunaexpanse.com>
2010-10-07 17:55:17 -0500

    starman starts as a process by "nobody"

--- bin/startqa.sh
+++ bin/startqa.sh
@@ -3,6 +3,6 @@ export PATH=/data/apps/bin:$PATH
 cd /data/Lacuna-Server/bin
 perl generate_docs.pl > /dev/null
 memcached -d -u nobody -m 512
-start_server --port 5000 -- starman --workers 10 --preload-app lacuna.psgi &
+start_server --port 5000 -- starman --user nobody --group nobody --workers 10 --preload-app lacuna.psgi &
 nginx -c /data/Lacuna-Server/etc/nginx.conf
 
