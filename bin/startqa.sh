dc3a3f5a2b1306e4d5435147d4ba0adb45e3c3f9
JT Smith <jt@lacunaexpanse.com>
2010-10-07 21:07:36 -0500

    rename them to what they really do

--- bin/startqa.sh
+++ bin/startqa.sh
@@ -3,7 +3,6 @@ export PATH=/data/apps/bin:$PATH
 cd /data/Lacuna-Server/bin
 perl generate_docs.pl > /dev/null
 memcached -d -u nobody -m 512
-plackup --port 5001 --workers 1 --user nobody --group nobody deploy.psgi &
 start_server --port 5000 -- starman --user nobody --group nobody --workers 10 --preload-app lacuna.psgi &
 nginx -c /data/Lacuna-Server/etc/nginx.conf
 
