ddf0e3fd2381e6a8bb03d0fd0e7b606465ea462a
JT Smith <jt@lacunaexpanse.com>
2010-10-20 14:57:41 -0500

    better payload handling

--- lib/Lacuna/DB/Result/Ships.pm
+++ lib/Lacuna/DB/Result/Ships.pm
@@ -270,7 +270,7 @@ sub pick_up_spies {
     my $payload = $self->payload;
     delete $payload->{fetch_spies};
     $payload->{spies} = \@riding;
-    #$self->payload($payload);
+    $self->payload($payload);
     $cargo_log->new({
         message     => 'after pick up spies',
         body_id     => $self->foreign_body_id,
@@ -285,13 +285,12 @@ sub handle_cargo_exchange {
     my $self = shift;
     if ($self->direction eq 'out') {
         $self->unload($self->payload, $self->foreign_body);
-        #$self->payload({});
         $self->turn_around;
         $self->pick_up_spies; # goes after turn around so we have the new date available
     }
     else {
         $self->unload($self->payload, $self->body);
-        #$self->payload({});
+        $self->payload({});
         $self->land;
     }
 }
