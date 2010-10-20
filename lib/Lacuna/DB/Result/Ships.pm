819e61b742404d9a8085acfceec973d317828adb
JT Smith <jt@lacunaexpanse.com>
2010-10-20 14:40:08 -0500

    more logging fo fetching spies

--- lib/Lacuna/DB/Result/Ships.pm
+++ lib/Lacuna/DB/Result/Ships.pm
@@ -237,8 +237,29 @@ sub pick_up_spies {
     foreach my $id (@{$self->payload->{fetch_spies}}) {
         my $spy = $spies->find($id);
         next unless defined $spy;
+    $cargo_log->new({
+        message     => 'found spy',
+        body_id     => $self->foreign_body_id,
+        data        => {spy => $id},
+        object_type => ref($self),
+        object_id   => $self->id,
+    })->insert;
         next unless $spy->is_available;
+    $cargo_log->new({
+        message     => 'spy is available',
+        body_id     => $self->foreign_body_id,
+        data        => {spy => $id},
+        object_type => ref($self),
+        object_id   => $self->id,
+    })->insert;
         next unless $spy->empire_id eq $empire_id;
+    $cargo_log->new({
+        message     => 'empire matches up',
+        body_id     => $self->foreign_body_id,
+        data        => {spy => $id},
+        object_type => ref($self),
+        object_id   => $self->id,
+    })->insert;
         push @riding, $spy->id;
         $spy->available_on($self->date_available);
         $spy->on_body_id($self->body_id);
