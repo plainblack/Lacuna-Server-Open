6e7076fd0459634b274854975c58d79dd7577d55
JT Smith <jt@lacunaexpanse.com>
2011-01-01 23:15:19 -0600

    grammar fix

--- lib/Lacuna/DB/Result/Building/Permanent/GeoThermalVent.pm
+++ lib/Lacuna/DB/Result/Building/Permanent/GeoThermalVent.pm
@@ -12,11 +12,11 @@ around can_build => sub {
     if ($body->get_plan(__PACKAGE__, 1)) {
         return $orig->($self, $body);  
     }
-    confess [1013,"You can't build an Geo Thermal Vent. It forms naturally."];
+    confess [1013,"You can't build a Geo Thermal Vent. It forms naturally."];
 };
 
 sub can_upgrade {
-    confess [1013, "You can't upgrade an Geo Thermal Vent. It forms naturally."];
+    confess [1013, "You can't upgrade a Geo Thermal Vent. It forms naturally."];
 }
 
 use constant image => 'geothermalvent';
