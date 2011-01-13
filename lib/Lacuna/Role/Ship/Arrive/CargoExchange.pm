89969e35c821b6137eb04f3b47c1f6ed713dc655
JT Smith <jt@lacunaexpanse.com>
2011-01-13 12:06:31 -0600

    fixed uninhabited bug

--- lib/Lacuna/Role/Ship/Arrive/CargoExchange.pm
+++ lib/Lacuna/Role/Ship/Arrive/CargoExchange.pm
@@ -5,7 +5,10 @@ use Moose::Role;
 
 after handle_arrival_procedures => sub {
     my ($self) = @_;
-    if ($self->direction eq 'out') {
+    if (!$self->foreign_body->empire_id) {
+        # do nothing, because it is uninhabited
+    }
+    elsif ($self->direction eq 'out') {
         $self->unload($self->foreign_body);
     }
     else {
