61b3747c10af9f008bef67f4f31f0fa2e6fa9355
JT Smith <jt@lacunaexpanse.com>
2011-01-13 11:26:30 -0600

    backgrounded, added trel

--- bin/run_hourly.sh
+++ bin/run_hourly.sh
@@ -9,5 +9,7 @@ perl generate_news_feeds.pl
 perl tick_spies.pl
 perl add_missions.pl
 perl clean_up_market.pl
+perl trelvestian/hourly_update.pl
+perl trelvestian/send_attack.pl&
 perl diablotin/hourly_update.pl
 perl saben/hourly_update.pl
