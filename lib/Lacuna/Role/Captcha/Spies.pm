05d95fec5edb9178679c134d0639c5e65793790f
Matthew Musgrove <mr.muskrat@gmail.com>
2011-03-08 08:04:01 -0600

    Testing why empire is undefined

--- lib/Lacuna/Role/Captcha/Spies.pm
+++ lib/Lacuna/Role/Captcha/Spies.pm
@@ -3,12 +3,32 @@ package Lacuna::Role::Captcha::Spies;
 use Moose::Role;
 
 before 'assign' => sub {
-	my ($self) = @_;
+	my ($self, $assignment) = @_;
+	die "spy id ", $self->id, " from body id ", $self->from_body->id, " empire id ", $self->empire_id;
 	my $empire = $self->from_body->empire;
+	
 	unless ($empire->current_session->check_captcha()) {
 		confess [1016,'Needs to solve a captcha.'];
 	}
 };
+
+=pod
+sub assign_spy {
+    my ($self, $session_id, $building_id, $spy_id, $assignment) = @_;
+    my $empire = $self->get_empire_by_session($session_id);
+    my $building = $self->get_building($empire, $building_id);
+    my $spy = $building->get_spy($spy_id);
+    unless (defined $spy) {
+        confess [1002, "Spy not found."];
+    }
+    my $mission = $spy->assign($assignment);
+    return {
+        status  => $self->format_status($empire, $building->body),
+        mission => $mission,
+        spy     => $spy->get_status,
+    };
+}
+=cut
 
 
 1;
