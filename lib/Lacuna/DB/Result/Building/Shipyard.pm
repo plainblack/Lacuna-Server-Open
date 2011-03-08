418b30de488e8d80ae1b7b2e4b010175e284c11b
JT Smith <jt@lacunaexpanse.com>
2011-03-08 14:56:50 -0600

    can build space stations

--- lib/Lacuna/DB/Result/Building/Shipyard.pm
+++ lib/Lacuna/DB/Result/Building/Shipyard.pm
@@ -78,9 +78,6 @@ sub can_build_ship {
     $ship->shipyard_id($self->id);
     my $ships = Lacuna->db->resultset('Lacuna::DB::Result::Ships');
     $costs ||= $self->get_ship_costs($ship);
-    if ($ship->type ~~ [qw(space_station)]) {
-        confess [1010, 'Not yet implemented.'];
-    }
     if ($self->level < 1) {
         confess [1013, "You can't build a ship if the shipyard isn't complete."];
     }
