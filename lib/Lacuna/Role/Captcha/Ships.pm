c986dff325fc81f2c79bd094e61bcd75c19f5d32
Matthew Musgrove <mr.muskrat@gmail.com>
2011-03-07 20:22:13 -0600

    Updated captcha roles

--- lib/Lacuna/Role/Captcha/Ships.pm
+++ lib/Lacuna/Role/Captcha/Ships.pm
@@ -5,7 +5,9 @@ use Moose::Role;
 before 'can_send_to_target' => sub {
     my ($self, $target) = @_;
 	my $empire = $self->body->empire;
-	$empire->current_session->check_captcha();
+	unless ($empire->current_session->check_captcha()) {
+		confess [1016,'Needs to solve a captcha.'];
+	}
 };
 
 1;
