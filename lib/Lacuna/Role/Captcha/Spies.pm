06f8a4e9170ab887bd3545a88f6c00a0fa558026
Matthew Musgrove <mr.muskrat@gmail.com>
2011-03-08 08:21:49 -0600

    Testing why session is undefined

--- lib/Lacuna/Role/Captcha/Spies.pm
+++ lib/Lacuna/Role/Captcha/Spies.pm
@@ -6,7 +6,7 @@ use Moose::Role;
 before 'assign' => sub {
 	my ($self, $assignment) = @_;
 	my $empire = $self->empire();
-	my $session = $empire->current_session(); # this is undefined?!??
+	my $session = $empire->current_session; # this is undefined?!??
 	unless ($session->check_captcha()) {
 		confess [1016,'Needs to solve a captcha.'];
 	}
@@ -17,8 +17,8 @@ before 'assign' => sub {
 before 'assign_spy' => sub {
     my ($self, $session_id, $building_id, $spy_id, $assignment) = @_;
     my $empire = $self->get_empire_by_session($session_id);
-	my $session = $empire->current_session();
-	unless ($session->check_captcha()) {
+#	my $session = $empire->current_session;
+	unless ($empire->current_session->check_captcha()) {
 		confess [1016,'Needs to solve a captcha.'];
 	}
 }
