82e3720e4546dfbef8294e72aa4dcb335cc4aa5b
JT Smith <jt@lacunaexpanse.com>
2011-03-08 16:28:20 -0600

    added roles

--- lib/Lacuna/DB/Result/Ships/SpaceStation.pm
+++ lib/Lacuna/DB/Result/Ships/SpaceStation.pm
@@ -11,14 +11,17 @@ use constant base_food_cost         => 108000;
 use constant base_water_cost        => 270000;
 use constant base_energy_cost       => 500000;
 use constant base_ore_cost          => 2000000;
-use constant base_time_cost         => 259200;
+use constant base_time_cost         => 60 * 60 * 72;
 use constant base_waste_cost        => 136900;
 use constant base_speed             => 10;
 use constant base_stealth           => 0;
 use constant base_hold_size         => 0;
 use constant pilotable              => 1;
 use constant build_tags             => [qw(War Intelligence)];
+use constant type_formatted         => 'Space Station Hull';
 
+with 'Lacuna::Role::Ship::Send::NotAllowed';
+with 'Lacuna::Role::Ship::Send::Uninhabited';
 
 no Moose;
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
