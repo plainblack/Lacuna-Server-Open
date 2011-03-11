7d6984afd8539603350d0e4c799707a080f9eea3
JT Smith <jt@lacunaexpanse.com>
2011-03-10 19:25:38 -0600

    Waste Recyclers now add 400 waste storage at base.

--- lib/Lacuna/DB/Result/Building/Waste/Recycling.pm
+++ lib/Lacuna/DB/Result/Building/Waste/Recycling.pm
@@ -101,7 +101,7 @@ use constant image => 'wasterecycling';
 use constant university_prereq => 3;
 
 use constant name => 'Waste Recycling Center';
-
+use constant waste_storage => 400;
 use constant food_to_build => 75;
 
 use constant energy_to_build => 75;
