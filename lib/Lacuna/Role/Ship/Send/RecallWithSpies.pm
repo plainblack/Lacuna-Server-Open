6bc97d6a23eeb25dca2d21e3e53e66071f2b2e8b
Matthew Musgrove <mr.muskrat@gmail.com>
2011-03-13 18:10:18 -0500

    Fix orbit and recall for spy shuttles

--- lib/Lacuna/Role/Ship/Send/RecallWithSpies.pm
+++ lib/Lacuna/Role/Ship/Send/RecallWithSpies.pm
@@ -1,32 +1,28 @@
-package Lacuna::Role::Ship::Send::LoadWithSpies;
+package Lacuna::Role::Ship::Send::RecallWithSpies;
 
 use strict;
 use Moose::Role;
 
 after send => sub {
     my $self = shift;
-    return if ( $self->direction eq 'in' ); # only load out when outbound
+    return if ( $self->direction eq 'out' ); # only load up on recall
     return if ( $self->payload->{spies}[0] ); # Spies loaded
     my $arrives = DateTime->now->add(seconds=>$self->calculate_travel_time($self->foreign_body));
     my @spies;
+    my $body = $self->body;
     foreach my $spy (@{$self->get_available_spies_to_send}) {
-        $spy->send($self->foreign_body_id, $arrives)->update;
+        $spy->send($body->id, $arrives)->update;
         push @spies, $spy->id;
     }
     $self->payload({ spies => \@spies });
     $self->update;
 };
 
-after can_send_to_target => sub {
-    my ($self, $target) = @_;
-    confess [ 1002, 'You have no idle spies to send.'] unless (scalar(@{$self->get_available_spies_to_send}));
-};
-
 sub get_available_spies_to_send {
     my $self = shift;
     my $body = $self->body;
     my $spies = Lacuna->db->resultset('Lacuna::DB::Result::Spies')->search(
-        {task => ['in','Idle','Training'], on_body_id=>$body->id, empire_id=>$body->empire->id},
+        {task => ['in','Idle','Training'], on_body_id=>$self->foreign_body_id, empire_id=>$body->empire->id},
     );
     my @spies;
     while (my $spy = $spies->next) {
