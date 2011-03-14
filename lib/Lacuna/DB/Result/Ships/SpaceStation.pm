1d4d1fc951b8a89bb1d9dec5db406347596cb01f
JT Smith <jt@lacunaexpanse.com>
2011-03-14 15:43:08 -0500

    better cost

--- lib/Lacuna/DB/Result/Ships/SpaceStation.pm
+++ lib/Lacuna/DB/Result/Ships/SpaceStation.pm
@@ -7,12 +7,12 @@ extends 'Lacuna::DB::Result::Ships';
 
 
 use constant prereq                 => { class=> 'Lacuna::DB::Result::Building::Embassy',  level => 20 };
-use constant base_food_cost         => 108000;
-use constant base_water_cost        => 270000;
-use constant base_energy_cost       => 500000;
-use constant base_ore_cost          => 2000000;
+use constant base_food_cost         => 400000;
+use constant base_water_cost        => 800000;
+use constant base_energy_cost       => 8000000;
+use constant base_ore_cost          => 8000000;
 use constant base_time_cost         => 60 * 60 * 72;
-use constant base_waste_cost        => 136900;
+use constant base_waste_cost        => 1000000;
 use constant base_speed             => 10;
 use constant base_stealth           => 0;
 use constant base_hold_size         => 0;
