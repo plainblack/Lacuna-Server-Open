f59f33a0d9c47e0cab4cd855506d75a7e22edef0
Graham Knop <haarg@haarg.org>
2011-03-20 22:22:01 -0500

    prevent timing exploit issues with resource pushing

--- lib/Lacuna/RPC/Building/Transporter.pm
+++ lib/Lacuna/RPC/Building/Transporter.pm
@@ -30,6 +30,13 @@ sub push_items {
     my $empire = $self->get_empire_by_session($session_id);
     my $building = $self->get_building($empire, $building_id);
     confess [1013, 'You cannot use a transporter that has not yet been built.'] unless $building->level > 0;
+    my $cache = Lacuna->cache;
+    if (! $cache->add('trade_push_lock', $building_id, 1, 5)) {
+        confess [1013, 'You have a push setup in progress.  Please wait a few moments and try again.'];
+    }
+    my $guard = guard {
+        $cache->delete('trade_push_lock',$building_id);
+    };
     unless ($empire->essentia >= 2) {
         confess [1011, "You need 2 essentia to push items using the Subspace Transporter."];
     }
