69e55c97157b2505ab1158752f5f70c54c2ee81f
JT Smith <jt@lacunaexpanse.com>
2011-03-21 14:40:40 -0500

    fix

---
+++ lib/Lacuna/DB/Result/Propositions/DemolishModule.pm
@@ -0,0 +1,26 @@
+package Lacuna::DB::Result::Propositions::DemolishModule;
+
+use Moose;
+use utf8;
+no warnings qw(uninitialized);
+extends 'Lacuna::DB::Result::Propositions';
+
+before pass => sub {
+    my ($self) = @_;
+    my $station = $self->station;
+    my $building = $station->buildings->find($self->scratch->{building_id});
+    if (defined $building) {
+        $building->demolish;
+    }
+    else {
+        $self->pass_extra_message('Unfortunately by the time the proposition passed, the building had already been demolished, effectively nullifying the vote.');
+    }
+};
+
+before fail => sub {
+    my ($self) = @_;
+};
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
