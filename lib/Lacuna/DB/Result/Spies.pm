e3a430b54a0b59006161110bee5887c5a5759557
JT Smith <jt@lacunaexpanse.com>
2011-03-29 12:47:54 -0500

    50 > 100

--- lib/Lacuna/DB/Result/Spies.pm
+++ lib/Lacuna/DB/Result/Spies.pm
@@ -1153,8 +1153,8 @@ sub can_conduct_advanced_missions {
     my $ranks = Lacuna->db->resultset('Lacuna::DB::Result::Log::Empire');
     my $defender_rank = $ranks->search({empire_id => $self->on_body->empire_id },{rows => 1})->get_column('empire_size_rank')->single;
     my $attacker_rank = $ranks->search({empire_id => $self->empire_id },{rows => 1})->get_column('empire_size_rank')->single;
-    unless ($attacker_rank + 50 > $defender_rank ) { # remember that the rank is inverted 1 is higher than 2.
-        confess [1010, 'This empire is more than 50 away from you in empire rank, and is therefore immune to this attack.'];
+    unless ($attacker_rank + 100 > $defender_rank ) { # remember that the rank is inverted 1 is higher than 2.
+        confess [1010, 'This empire is more than 100 away from you in empire rank, and is therefore immune to this attack.'];
     }
     return 1;
 }
