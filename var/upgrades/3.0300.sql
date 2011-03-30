a2027095823835f74ea4374fba992fb935f0373e
Matthew Musgrove <mr.muskrat@gmail.com>
2011-03-29 21:47:36 -0500

    Fix upgrade scripts to what they should be.

--- var/upgrades/3.0300.sql
+++ var/upgrades/3.0300.sql
@@ -1,11 +1,2 @@
-CREATE TABLE `mercenary_market` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_offered` datetime NOT NULL,
-  `body_id` int(11) NOT NULL,
-  `ship_id` int(11) NOT NULL,
-  `ask` float(11,1) NOT NULL,
-  `cost` float(11,1) NOT NULL,
-  `payload` mediumblob NOT NULL,
-  PRIMARY KEY (`id`)
-);
+alter table market add column has_spy tinyint default 0;
 
--- var/upgrades/3.0400.sql
+++ var/upgrades/3.0400.sql
@@ -16,3 +16,14 @@ CREATE TABLE `laws` (
 );
 alter table star add column station_id int;
 alter table star add foreign key star_fk_alliance_id (station_id) references body (id) on delete set null;
+alter table market drop column has_spy;
+CREATE TABLE `mercenary_market` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_offered` datetime NOT NULL,
+  `body_id` int(11) NOT NULL,
+  `ship_id` int(11) NOT NULL,
+  `ask` float(11,1) NOT NULL,
+  `cost` float(11,1) NOT NULL,
+  `payload` mediumblob NOT NULL,
+  PRIMARY KEY (`id`)
+);
