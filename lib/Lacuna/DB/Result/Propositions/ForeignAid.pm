a717d24c3b1fd5fcf84ca4c5265b6d4ddd31a9be
Matthew Musgrove <mr.muskrat@gmail.com>
2011-04-03 22:17:00 -0500

    Working foreign aid.

--- lib/Lacuna/DB/Result/Propositions/ForeignAid.pm
+++ lib/Lacuna/DB/Result/Propositions/ForeignAid.pm
@@ -2,14 +2,32 @@ package Lacuna::DB::Result::Propositions::ForeignAid;
 
 use Moose;
 use utf8;
+use DateTime;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Propositions';
 
-# BUG Need to actually send the foreign aid
 before pass => sub {
     my ($self) = @_;
-    my $station = $self->station;
-    $self->station->add_news(200, $self->scratch->{message});
+    my $resources = $self->scratch->{resources};
+    my $planet_id = $self->scratch->{planet_id};
+    my $planet = Lacuna->db->resultset('Lacuna::DB::Result::Map::Body')->find($planet_id);
+    if (defined $planet) {
+        if ($planet->star->station_id == $self->body_id) {
+            my $finish = DateTime->now;
+            my $supply_pod = Lacuna->db->resultset('Lacuna::DB::Result::Ships')->new({
+                body_id         => $self->station_id,
+                type            => 'supply_pod',
+                hold_size       => $self->scratch->{resource},
+                date_available  => $finish,
+            });
+            $supply_pod->send(target => $planet);
+        }
+        else {
+        }
+    }
+    else {
+    }
+
 };
 
 no Moose;
