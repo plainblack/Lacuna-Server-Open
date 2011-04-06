431f0b9a8de6b0df969b59108c05bfd74d433dcb
Matthew Musgrove <mr.muskrat@gmail.com>
2011-04-05 21:04:11 -0500

    Fix: Abandoning Space Stations will now work.

--- lib/Lacuna/DB/Result/Propositions/AbandonStation.pm
+++ lib/Lacuna/DB/Result/Propositions/AbandonStation.pm
@@ -5,7 +5,7 @@ use utf8;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Propositions';
 
-before pass => sub {
+after pass => sub {
     my ($self) = @_;
     $self->station->sanitize;
 };
