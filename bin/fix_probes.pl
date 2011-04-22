0372efbb4aac43399467c24e8863c8fcc7e48155
Matthew Musgrove <mr.muskrat@gmail.com>
2011-04-21 20:27:40 -0500

    Started work on a script to fix the probes on insurrected planets.

--- bin/fix_probes.pl
+++ bin/fix_probes.pl
@@ -19,21 +19,20 @@ my $start = time;
 out('Loading DB');
 our $db = Lacuna->db;
 
-# select p.id, p.empire_id, p.star_id, p.body_id, p.alliance_id, b.empire_id from probes as p, body as b where p.body_id = b.id and p.empire_id != ( select b.empire_id from body as b where b.id = p.body_id );
-
+# SELECT me.id, me.empire_id, me.star_id, me.body_id, me.alliance_id FROM probes me JOIN body body ON body.id = me.body_id WHERE ( ( body.empire_id != me.empire_id AND body.id = me.body_id ) ) ORDER BY body.id; 
 my $bad = $db->resultset('Lacuna::DB::Result::Probes')->search(
     {
-        'body.id' => { '=' => 'probe.body_id' },
-        'body.empire_id' => { '!=' => 'probe.empire_id' },
+        'me.body_id' => { '=' => 'body.id' },
+        'me.empire_id' => { '!=' => 'body.empire_id' },
     },
     {
         join => [ qw/body/ ],
-        order_by => [ qw/body.id body.empire_id/ ],
+        order_by => [ qw/body.id/ ],
     }
 );
 
 while (my $probe = $bad->next) {
-    print $probe->id, ' ', $probe->empire_id, ' ', $probe->star_id, ' ', $probe->body_id, "\n";
+    print join( ', ', $probe->id, $probe->empire_id, $probe->star_id,  $probe->body_id ), "\n";
 }
 
 my $finish = time;
