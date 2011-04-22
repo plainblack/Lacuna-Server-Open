310e8759f180ec446f6a9a61ff1732c288ca401e
Matthew Musgrove <mr.muskrat@gmail.com>
2011-04-21 19:18:26 -0500

    Fix run_weekly.sh

--- bin/run_weekly.sh
+++ bin/run_weekly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/data/apps/bin:$PATH
 cd /data/Lacuna-Server/bin
-perl weekly_medals.pl
+perl weekly_medals.pl >> /tmp/weekly_medals.log 2>> /tmp/weekly_medals.log
 perl trelvestian/reset_essentia_veins.pl
 perl clean_up_mail.pl
