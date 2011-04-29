d0c599f90f3ccc1a27c6df1fc96d62edd556f609
Matthew Musgrove <mr.muskrat@gmail.com>
2011-04-29 16:47:47 -0500

    Add clean_up_game_over.pl

---
+++ bin/clean_up_game_over.pl
@@ -0,0 +1,47 @@
+use 5.010;
+use strict;
+use feature "switch";
+use lib '/data/Lacuna-Server/lib';
+use Lacuna::DB;
+use Lacuna;
+use List::Util qw(shuffle);
+use Lacuna::Util qw(randint format_date);
+use Getopt::Long;
+$|=1;
+our $quiet;
+GetOptions(
+    'quiet'         => \$quiet,  
+);
+
+
+out('Started');
+my $start = DateTime->now;
+
+out('Loading DB');
+our $db = Lacuna->db;
+my $empires = $db->resultset('Lacuna::DB::Result::Empire');
+
+
+out('Deleting Empires');
+while (my $empire = $empires->next) {
+    out('Empire ', $empire->name);
+#    $empire->delete;    
+}
+
+my $finish = time;
+out('Finished');
+out((($finish - $start->epoch)/60)." minutes have elapsed");
+
+
+###############
+## SUBROUTINES
+###############
+
+sub out {
+    my $message = shift;
+    unless ($quiet) {
+        say format_date(DateTime->now), " ", $message;
+    }
+}
+
+
