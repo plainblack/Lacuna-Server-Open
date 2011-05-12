72e97df99d809f41023e6d2d22cfcdd3d12c4578
Matthew Musgrove <mr.muskrat@gmail.com>
2011-05-11 22:30:27 -0500

    Fix: Party happiness will no longer disappear a second after being awarded.

--- lib/Lacuna/DB/Result/Building/Park.pm
+++ lib/Lacuna/DB/Result/Building/Park.pm
@@ -54,7 +54,7 @@ sub throw_a_party {
 before finish_work => sub {
     my $self = shift;
     my $planet = $self->body;
-    $planet->add_happiness($self->work->{happiness_from_party});
+    $planet->add_happiness($self->work->{happiness_from_party})->update;
 };
 
 use constant controller_class => 'Lacuna::RPC::Building::Park';
