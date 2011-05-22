949cb2440c3f3b778fa9f8a4b69f85c11d2c926a
Matthew Musgrove <mr.muskrat@gmail.com>
2011-05-22 14:39:43 -0500

    Add script to add multiple plans to a single body

---
+++ bin/util/add_plans.pl
@@ -0,0 +1,51 @@
+use 5.010;
+use strict;
+use lib '/data/Lacuna-Server/lib';
+use Lacuna::DB;
+use Lacuna;
+use Lacuna::Util qw(randint format_date);
+use Getopt::Long;
+$|=1;
+our $quiet;
+our $body_id;
+our $class;
+our $count;
+GetOptions(
+    'quiet'         => \$quiet,  
+    'body'          => \$body_id,
+    'class'          => \$class,
+    'count'         => \$count,
+);
+
+out('Started');
+my $start = time;
+
+out('Loading DB');
+our $db = Lacuna->db;
+
+my $body = $db->resultset('Lacuna::DB::Result::Map::Body')->find($body_id);
+unless ($body) {
+    confess "Cannot find body id $body_id";
+}
+say "Adding $count level 1 $class plans to $body->name";
+for my $cnt ( 1 .. $count ) {
+    $body->add_plan($class, 1, 0);
+}
+
+my $finish = time;
+out('Finished');
+out((($finish - $start)/60)." minutes have elapsed");
+
+
+###############
+## SUBROUTINES
+###############
+
+sub out {
+    my $message = shift;
+    unless ($quiet) {
+        say format_date(DateTime->now), " ", $message;
+    }
+}
+
+
