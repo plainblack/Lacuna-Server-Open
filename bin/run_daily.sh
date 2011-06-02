fba59fb056ae9795a74e7a51fe33a24b43ebfa8a
Matthew Musgrove <mr.muskrat@gmail.com>
2011-06-01 22:32:26 -0500

    move rotate_taxes_paid.pl up

--- bin/run_daily.sh
+++ bin/run_daily.sh
@@ -4,8 +4,8 @@ cd /data/Lacuna-Server/bin
 perl trickle_essentia_veins.pl
 perl determine_lottery_winner.pl
 perl alert_inactive_users.pl
+perl rotate_taxes_paid.pl
 perl diablotin/send_attack.pl --randomize&
 #perl saben/send_attack.pl --randomize&
 #perl trelvestian/check_for_total_victory.pl
-perl rotate_taxes_paid.pl
 
