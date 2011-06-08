ca629de2f20f138b5d1f459958f35044d3968890
Matthew Musgrove <mr.muskrat@gmail.com>
2011-06-07 23:38:44 -0500

    Fix: Tax payments will be properly rotated.

--- bin/rotate_taxes_paid.pl
+++ bin/rotate_taxes_paid.pl
@@ -1,9 +1,9 @@
 use 5.010;
 use strict;
 use lib '/data/Lacuna-Server/lib';
-use Lacuna::DB;
 use Lacuna;
 use Lacuna::Util qw(format_date);
+use DBI;
 use Getopt::Long;
 $|=1;
 
@@ -17,10 +17,9 @@ out('Started');
 my $start = time;
 
 out('Loading DB');
-our $db = Lacuna->db;
-
-advance();
-
+my $config = Lacuna->config->get('db');
+my $db = DBI->connect($config->{dsn}, $config->{username}, $config->{password});
+$db->do("update taxes set paid_6 = paid_5, paid_5 = paid_4, paid_4 = paid_3, paid_3 = paid_2, paid_2 = paid_1, paid_1 = paid_0, paid_0 = 0");
 
 my $finish = time;
 out('Finished');
@@ -30,11 +29,6 @@ out((($finish - $start)/60)." minutes have elapsed");
 ###############
 ## SUBROUTINES
 ###############
-
-sub advance {
-    $db->do("update taxes set paid_6 = paid_5, paid_5 = paid_4, paid_4 = paid_3, paid_3 = paid_2, paid_2 = paid_1, paid_1 = paid_0, paid_0 = 0");
-}
-
 
 # UTILITIES
 
