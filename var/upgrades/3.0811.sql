d7e1a579f63f21b5cdce446c5a2ff941ddeb0445
Matthew Musgrove <mr.muskrat@gmail.com>
2011-06-21 18:04:17 -0500

    Add in defending body name

--- var/upgrades/3.0811.sql
+++ var/upgrades/3.0811.sql
@@ -9,6 +9,7 @@ CREATE TABLE `battle_log` (
   `defending_empire_id` int(11) NOT NULL,
   `defending_empire_name` char(30) NOT NULL,
   `defending_body_id` int(11) NOT NULL,
+  `defending_body_name` char(30) NOT NULL,
   `defending_unit_name` char(30) NOT NULL,
   `victory_to` char(8) NOT NULL,
   PRIMARY KEY (`id`)
