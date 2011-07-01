033994a21d2a353cbb0cf25a18e72701b71257a4
Matthew Musgrove <mr.muskrat@gmail.com>
2011-07-01 01:04:38 -0500

    Change defending empire to be nullable

--- var/upgrades/3.0812.sql
+++ var/upgrades/3.0812.sql
@@ -1,2 +1,2 @@
-alter table battle_log change column `defending_empire_id` int(11);
-alter table battle_log change column `defending_empire_name` int(11);
+alter table battle_log change column defending_empire_id defending_empire_id int(11);
+alter table battle_log change column defending_empire_name defending_empire_name int(11);
