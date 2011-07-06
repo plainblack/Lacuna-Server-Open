f6652ff546e5cd37082709fce6d7052d12bbd694
Matthew Musgrove <mr.muskrat@gmail.com>
2011-07-06 00:02:55 -0500

    fix victory conditions

--- bin/trelvestian/check_for_total_victory.pl
+++ bin/trelvestian/check_for_total_victory.pl
@@ -26,7 +26,7 @@ my $empires = Lacuna->db->resultset('Lacuna::DB::Result::Empire');
 my $cache = Lacuna->cache;
 my $lec = Lacuna::DB::Result::Empire->lacuna_expanse_corp;
 
-if ($cache->get('FourTrel') ne 'Tournament Over') {
+if ($cache->get('tournament', 'FourTrel') ne 'Tournament Over') {
 	out('Checking victory planets.');
 	my %victory_points;
 	my $victory_empire;
@@ -34,50 +34,58 @@ if ($cache->get('FourTrel') ne 'Tournament Over') {
 	foreach my $id (@{Lacuna->config->get('win/alliance_control')}) {
 	    my $planet = $bodies->find($id);
 	    if ($planet->empire_id eq '' || $planet->empire_id > 1) {
-		if ($cache->get('victory_planet'.$id) ne $planet->empire_id) {
-		    my $controlled = '{Starmap '.$planet->x.' '.$planet->y.' '.$planet->name.'}';
-		    my $controller = $planet->empire_id ? '{Empire '.$planet->empire_id.' '.$planet->empire->name.'}' : 'No one';
-		    while (my $empire = $empires->next) {
-			$empire->send_message(
-			    tags        => ['Alert'],
-			    from        => $lec,
-			    body        => $controller.q{ now controls the Trelvestian planet named }.$controlled.q{.},
-			    subject     => 'Control Changed Hands',
-			);
-		    }
-		}
-		if ($planet->empire_id) {
-		    $victory_points{$planet->empire_id}++;
-		    $victory_empire = $planet->empire;
-		}
+            if ($cache->get('victory_planet'.$id) ne $planet->empire_id) {
+                my $controlled = '{Starmap '.$planet->x.' '.$planet->y.' '.$planet->name.'}';
+                my $controller = $planet->empire_id ? '{Empire '.$planet->empire_id.' '.$planet->empire->name.'}' : 'No one';
+                while (my $empire = $empires->next) {
+                    $empire->send_message(
+                        tags        => ['Alert'],
+                        from        => $lec,
+                        body        => $controller.q{ now controls the Trelvestian planet named }.$controlled.q{.},
+                        subject     => 'Control Changed Hands',
+                    );
+                }
+            }
+            if ($planet->empire_id) {
+                $victory_points{$planet->empire_id}++;
+                $victory_empire = $planet->empire;
+            }
 	    }
 	    $cache->set('victory_planet'.$id, $planet->empire_id, 60 * 60 * 24 * 7);
 	}
 
 	if (defined $victory_empire) {
 	    if ($victory_points{$victory_empire->id} >= 4) {
-		if ($server_url =~ /us2/) {
-		    $cache->set('server','status','Game Over', 60 * 60 * 24 * 30);
-		}
-		elsif ($server_url =~ /us1/) {
-		    $cache->set('FourTrel','Tournament Over', 60 * 60 * 24 * 30);
-		    my $alliance = Lacuna->db->resultset('Lacuna::DB::Result::Alliance')->find($victory_empire->alliance_id);
-		    if (defined $alliance) {
-			while (my $empire = $alliance->members->next) {
-			    $empire->add_medal('TrelDefeated');
-			    $empire->add_medal('TournamentVictory');
-			}
-			set_announcement("The '" . $alliance->name . "' alliance has won the Four Trel Colonies tournament!")
-		    } 
-		    else {
-			$victory_empire->add_medal('TrelDefeated');
-			$victory_empire->add_medal('TournamentVictory');
-			set_announcement("The '" . $victory_empire->name . "' empire has won the Four Trel Colonies tournament single-handedly!")
-		    }
-		}
-		else {
-		    out('Not configured to run on ' . $server_url);
-		}
+            if ($server_url =~ /us2/) {
+                $cache->set('server','status','Game Over', 60 * 60 * 24 * 30);
+            }
+            elsif ($server_url =~ /us1/) {
+                $cache->set('tournament', 'FourTrel','Tournament Over', 60 * 60 * 24 * 30);
+                out('victory empire: ' . $empire->name);
+                my $alliance = Lacuna->db->resultset('Lacuna::DB::Result::Alliance')->find($victory_empire->alliance_id);
+                if (defined $alliance) {
+                    out('victory alliance: ' . $alliance->name);
+                    my @allies = $alliance->members->get_column('id')->all;
+                    for my $id ( @allies ) {
+                        my $allie = $empires->find($id);
+                        out('Giving medals to ' . $allie->name);
+                        $allie->add_medal('TrelDefeated');
+                        $allie->add_medal('TournamentVictory');
+                    }
+                    out('setting announcement');
+                    set_announcement("The '" . $alliance->name . "' alliance has won the Four Trel Colonies tournament!")
+                } 
+                else {
+                    out('Giving medals to ' . $victory_empire->name);
+                    $victory_empire->add_medal('TrelDefeated');
+                    $victory_empire->add_medal('TournamentVictory');
+                    out('setting announcement');
+                    set_announcement("The '" . $victory_empire->name . "' empire has won the Four Trel Colonies tournament single-handedly!")
+                }
+            }
+            else {
+                out('Not configured to run on ' . $server_url);
+            }
 	    }
 	}
 }
