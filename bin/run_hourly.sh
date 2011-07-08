964fe2a1fdbf35ab4d45a4c8fd21c97fcee1ee6c
Matthew Musgrove <mr.muskrat@gmail.com>
2011-07-07 21:32:07 -0500

    change where things get logged

--- bin/run_hourly.sh
+++ bin/run_hourly.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 export PATH=/data/apps/bin:$PATH
 cd /data/Lacuna-Server/bin
-perl clean_up_empires.pl
-perl sanitize_ss.pl
-perl tick_planets.pl
-perl summarize_server.pl
-perl summarize_economy.pl
-perl generate_news_feeds.pl
-perl tick_spies.pl
-perl tick_parliament.pl
-perl add_missions.pl
-perl clean_up_market.pl
-perl trelvestian/hourly_update.pl
-perl trelvestian/send_attack.pl&
-perl diablotin/hourly_update.pl
-perl saben/hourly_update.pl
-perl check_for_total_victory.pl
+perl clean_up_empires.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl sanitize_ss.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl tick_planets.pl >>/tmp/tick_planets.log 2>>/tmp/tick_planets.log
+perl summarize_server.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl summarize_economy.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl generate_news_feeds.pl >>/tmp/news_feeds.log 2>>/tmp/news_feeds.log
+perl tick_spies.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl tick_parliament.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl add_missions.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl clean_up_market.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl trelvestian/hourly_update.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl trelvestian/send_attack.pl >>/tmp/hourly.log 2>>/tmp/hourly.log &
+perl diablotin/hourly_update.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl saben/hourly_update.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
+perl check_for_total_victory.pl >>/tmp/hourly.log 2>>/tmp/hourly.log
