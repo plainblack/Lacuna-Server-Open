1542bd93757df11456e9b6258da5523523d28fa9
lemming552 <lemming@quirkyqatz.com>
2011-09-25 17:04:57 -0600

    Limit longest extended build time to 60 days.

--- lib/Lacuna/DB/Result/Building.pm
+++ lib/Lacuna/DB/Result/Building.pm
@@ -744,6 +744,7 @@ sub cost_to_upgrade {
     $time_inflator = 1 if ($time_inflator < 1);
     my $throttle = Lacuna->config->get('building_build_speed') || 6;
     my $time_cost = (($self->level+1)/$throttle * $self->time_to_build * $time_inflator ** INFLATION) * $self->time_cost_reduction_bonus * $oversight_reduction;
+    $time_cost = 5184000 if ($time_cost > 5184000); #60 days
     $time_cost = 15 if ($time_cost < 15);
     return {
         food    => sprintf('%.0f',$self->food_to_build * $upgrade_cost * $upgrade_cost_reduction),
