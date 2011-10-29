eff2bdd8e7ac2fd4a102a39177ceb84b494ebcc2
lemming552 <lemming@quirkyqatz.com>
2011-10-29 16:57:49 -0600

    Increase instances to 9 each.

--- lib/Lacuna/DB/Result/Building/Permanent/Lagoon.pm
+++ lib/Lacuna/DB/Result/Building/Permanent/Lagoon.pm
@@ -30,7 +30,7 @@ around produces_food_items => sub {
     return $foods;
 };
 use constant water_production => 0.1;
-use constant max_instances_per_planet => 1;
+use constant max_instances_per_planet => 9;
 
 no Moose;
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
