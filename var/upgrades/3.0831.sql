ff8adef52e58cb9a8604bfb3339af9c589c78516
lemming552 <lemming@quirkyqatz.com>
2012-01-01 00:31:23 -0700

    Alliance Mapping data made available.

---
+++ var/upgrades/3.0831.sql
@@ -0,0 +1,4 @@
+alter table colony_log add column body_id int(11) not null default 0;
+alter table colony_log add column x int(11) not null default 0;
+alter table colony_log add column y int(11) not null default 0;
+alter table colony_log add column zone varchar(16) not null default 0;
