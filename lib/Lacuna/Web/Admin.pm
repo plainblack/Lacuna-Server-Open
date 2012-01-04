fbbb7c3563f972a037f4437a74cbc6aa9a6e6a2a
ian docherty <icyde@iandocherty.com>
2012-01-04 07:13:58 -0500

    Improved display of AI attack/retaliate numbers

--- lib/Lacuna/Web/Admin.pm
+++ lib/Lacuna/Web/Admin.pm
@@ -1300,11 +1300,19 @@ ATTACKER:
         my $snarks      = $scratchpad->{attack}{$attacker}{snarks};
         my $scows       = $scratchpad->{attack}{$attacker}{scows};
         my $frequency   = $scratchpad->{attack}{$attacker}{frequency};
-        my $counter     = defined $attack{$attacker} ? $attack{$attacker} : {attack_victories=>0, defense_victories=>0, attack_spy_hours=>0, weight=>0};
+        my $counter = {attack_victories=>0, defense_victories=>0, attack_spy_hours=>0, weight=>0};
+        if (defined $attack{$attacker}) {
+            $counter = {
+                attack_victories  => $attack{$attacker}{attack_victories},
+                defense_victories => $attack{$attacker}{defense_victories},
+                attack_spy_hours  => $attack{$attacker}{attack_spy_hours},
+                weight            => $attack{$attacker}{weight},
+            };
+        }
         $out .= "<tr><td>".$attack_empire->name."</td><td>".$defence{$attacker}{attack_victories}."</td><td>".$defence{$attacker}{defense_victories}."</td>";
         $out .= "<td>".$defence{$attacker}{attack_spy_hours}."</td><td>".$defence{$attacker}{weight}."</td>";
-        $out .= "<td>".$counter{$attacker}{attack_victories}."</td><td>".$counter{$attacker}{defense_victories}."</td>";
-        $out .= "<td>".$counter{attack_spy_hours}."</td><td>".$counter{weight}."</td>";
+        $out .= "<td>".$counter->{attack_victories}."</td><td>".$counter->{defense_victories}."</td>";
+        $out .= "<td>".$counter->{attack_spy_hours}."</td><td>".$counter->{weight}."</td>";
         $out .= "<form action='/admin/delambert_war'>";
         $out .= "<td><select name='colony_id'>";
         foreach my $colony (@colonies) {
@@ -1326,9 +1334,10 @@ ATTACKER:
     }
     $out .= "</table>\n";
     $out .= "<ul>\n";
-    $out .= "<li>Victories, Defeats and Spy hours are attacks against the DeLamberti</li>";
-    $out .= "<li>Retaliate Weight, is the factor which measures the AI Retaliation against those attacks</li>";
+    $out .= "<li>A-Victories, A-Defeats and A-Spy hours are attacks against the DeLamberti</li>";
+    $out .= "<li>R-Victories, R-Defeats and R-Spy hours are retaliations by the DeLamberti</li>";
     $out .= "<li>Attack Weight, is a measure of the amount of attacks against the AI</li>";
+    $out .= "<li>Retaliate Weight, is a measure of the AI Retaliation against those attacks</li>";
     $out .= "<li>The list is sorted so that those empires with the highest (Attack Weight - Retaliate Weight) are first</li>";
     $out .= "</ul>\n";
 
