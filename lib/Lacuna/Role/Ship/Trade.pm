b74d886c2783c10fd86773301563216709913bfe
lemming552 <move@quirkyqatz.com>
2012-01-06 17:09:10 -0700

    Fixed oops on payload_ships_id.

--- lib/Lacuna/Role/Ship/Trade.pm
+++ lib/Lacuna/Role/Ship/Trade.pm
@@ -27,7 +27,7 @@ sub check_payload_ships_id {
 
     return if not $items;
 
-    my $ship_count = grep {$_->{type} eq 'ship'} @$items;
+    my $ship_count = scalar @$items;
 
     $self->_can_target_accept_ships($target, $ship_count);
 }
