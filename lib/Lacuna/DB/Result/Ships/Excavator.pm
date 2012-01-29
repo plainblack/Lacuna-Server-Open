9ed4ec449749bd1c87a39182cf7df8dd7cd61019
lemming552 <lemming@quirkyqatz.com>
2012-01-29 14:02:19 -0700

    Rethought excav ship costs.

--- lib/Lacuna/DB/Result/Ships/Excavator.pm
+++ lib/Lacuna/DB/Result/Ships/Excavator.pm
@@ -14,12 +14,12 @@ use constant prereq                 => { class=> 'Lacuna::DB::Result::Building::
 # use constant base_time_cost         => 20000;
 # use constant base_waste_cost        => 1200;
 # use constant base_speed             => 1800;
-use constant base_food_cost         => 100000;
-use constant base_water_cost        => 200000;
-use constant base_energy_cost       => 500000;
-use constant base_ore_cost          => 900000;
+use constant base_food_cost         => 3000;
+use constant base_water_cost        => 5000;
+use constant base_energy_cost       => 20000;
+use constant base_ore_cost          => 80000;
 use constant base_time_cost         => 8 * 60 * 60;
-use constant base_waste_cost        => 300000;
+use constant base_waste_cost        => 10000;
 use constant base_speed             => 1000;
 use constant base_stealth           => 0;
 use constant base_hold_size         => 0;
