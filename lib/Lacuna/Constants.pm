6f7191a14306ae276fbd6bd0acb47614ee718dc2
lemming552 <lemming@quirkyqatz.com>
2012-04-03 10:05:28 -0600

    Forgot one place to modify names.

--- lib/Lacuna/Constants.pm
+++ lib/Lacuna/Constants.pm
@@ -109,7 +109,7 @@ use constant SPACE_STATION_MODULES => (qw(
     ));
 use constant SHIP_TYPES => (qw( probe short_range_colony_ship colony_ship spy_pod cargo_ship space_station 
                              smuggler_ship mining_platform_ship terraforming_platform_ship surveyor
-                             gas_giant_settlement_ship scow scow_fast scow_20 scow_30 dory freighter snark snark2 snark3 thud
+                             gas_giant_settlement_ship scow scow_fast scow_large scow_mega dory freighter snark snark2 snark3 thud
                              supply_pod supply_pod2 supply_pod3 supply_pod4
                              drone fighter spy_shuttle observatory_seeker security_ministry_seeker 
                              spaceport_seeker excavator detonator scanner barge hulk hulk_fast hulk_huge galleon stake
