30e9378bfc30dafec104797134fe7a851cd655fe
ysth <sthoenna@gmail.com>
2012-04-17 00:03:59 -0700

    Asteroids shouldn't have water

--- lib/Lacuna/DB/Result/Map/Body/Asteroid/A18.pm
+++ lib/Lacuna/DB/Result/Map/Body/Asteroid/A18.pm
@@ -8,8 +8,8 @@ extends 'Lacuna::DB::Result::Map::Body::Asteroid';
 use constant image => 'a18';
 
 use constant trona => 3326;
+
 use constant gypsum => 4120;
-use constant water => 115;
 
 no Moose;
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
