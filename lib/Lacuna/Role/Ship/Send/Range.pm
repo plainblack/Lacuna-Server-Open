02a97d08160e36153f05dc89dd76010a2155db1a
lemming552 <lemming@quirkyqatz.com>
2012-05-09 14:40:50 -0600

    Sql added to modify existing SRCS.

--- lib/Lacuna/Role/Ship/Send/Range.pm
+++ lib/Lacuna/Role/Ship/Send/Range.pm
@@ -6,10 +6,10 @@ use Moose::Role;
 after can_send_to_target => sub {
     my ($self, $target) = @_;
 
-    my $distance = $self->body->calculate_distance_to_target($target);
+    my $distance = int($self->body->calculate_distance_to_target($target) +0.5);
     my $range = int($self->speed/100+0.5);
     if ($distance > $range) {
-      confess [1009, 'You only have a range of '.$range.' with this ship.'];
+      confess [1009, 'You only have a range of '.$range.' with this ship. This body is '.$distance.' away.'];
     }
 };
 
---
+++ var/upgrades/3.0851.sql
@@ -0,0 +1,2 @@
+
+update ships set speed = 700 where type = 'short_range_colony_ship';
