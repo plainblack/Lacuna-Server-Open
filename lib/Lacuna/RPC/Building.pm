026eb768eff7ebf356dd19d5ee8044bd6fd578e7
Ian Docherty <github@iandocherty.com>
2012-07-23 15:30:51 -0400

    Removed some diagnostics

--- lib/Lacuna/RPC/Building.pm
+++ lib/Lacuna/RPC/Building.pm
@@ -83,9 +83,7 @@ sub view {
     my $self = shift;
     my $args = shift;
         
-print STDERR Dumper($args);
-
-if (ref($args) ne "HASH") {
+    if (ref($args) ne "HASH") {
         $args = {
             session         => $args,
             building        => shift,
