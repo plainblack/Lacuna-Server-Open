c3f563a8dd1e92230f39e6197d713412beea3722
Ian Docherty <github@iandocherty.com>
2012-08-30 04:25:42 -0400

    Revert "Updated to use building cache and to allow script to run as a cron job"
    This reverts commit 0b7420219d416a9a7c8147b7c744d6466b2c2c9e.
    
    Conflicts:
    
    	bin/util/revamp_map.pl

--- var/upgrades/3.0866.sql
+++
@@ -1,7 +0,0 @@
-create table config (
-    id              int(11) not null auto_increment,
-    name            varchar(30) not null,
-    value           varchar(256),
-    primary key (id),
-    key c_idx_key (name)
-);
