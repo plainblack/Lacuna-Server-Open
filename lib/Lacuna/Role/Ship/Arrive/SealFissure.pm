163f86546ae26646b3a0dbb0c4152b45c8b35707
lemming552 <move@quirkyqatz.com>
2012-11-29 22:48:00 -0700

    Using ore amount for sealing.

--- lib/Lacuna/Role/Ship/Arrive/SealFissure.pm
+++ lib/Lacuna/Role/Ship/Arrive/SealFissure.pm
@@ -4,6 +4,7 @@ use strict;
 use Moose::Role;
 use Lacuna::Util qw(randint);
 use DateTime;
+use Lacuna::Constants qw(FOOD_TYPES ORE_TYPES);
 
 after handle_arrival_procedures => sub {
     my ($self) = @_;
@@ -14,6 +15,13 @@ after handle_arrival_procedures => sub {
     # Turn around if occupied
     return if ($self->foreign_body->empire_id);
 
+    my $amount = 0;
+    my $payload = $self->payload;
+    foreach my $type (ORE_TYPES) {
+        if (defined($payload->{resources}{$type})) {
+            $amount += $payload->{resources}{$type};
+        }
+    }
     # determine target building
     my $building;
     my $body_hit = $self->foreign_body;
@@ -24,7 +32,7 @@ after handle_arrival_procedures => sub {
 
     # handle fissure
     if (defined $fissure) {
-        my $curr_eff = $fissure->efficiency + randint(1,5);
+        my $curr_eff = $fissure->efficiency + randint(1,5) + int($amount/1_000_000);
         $curr_eff = 100 if $curr_eff > 100;
         my $curr_lev = $fissure->level;
 
@@ -34,6 +42,7 @@ after handle_arrival_procedures => sub {
 
         if ( $down_chance > randint(0,99) ) {
             $curr_lev--;
+            $curr_eff = 10;
         }
         if ($curr_lev < 1) {
             $fissure->delete;
@@ -41,6 +50,7 @@ after handle_arrival_procedures => sub {
                 tags        => ['Alert'],
                 filename    => 'we_destroyed_a_fissure.txt',
                 params      => [$self->type_formatted, $body_hit->x, $body_hit->y, $body_hit->name ],
+            );
         }
         else {
             $fissure->level($curr_lev);
@@ -58,6 +68,35 @@ after handle_arrival_procedures => sub {
     }
     
     confess [-1];
+};
+
+after send => sub {
+    my $self = shift;
+    my $part = $self->hold_size;
+    my $ore = $part;
+    my $body = $self->body;
+    my $payload;
+    my $ore_stored = 0;
+    my $ore_type_count = 0;
+    for my $type (ORE_TYPES) {
+      my $stored = $body->type_stored($type);
+      $ore_stored += $stored;
+      $ore_type_count++ if ($stored);
+    }
+    $ore = $ore_stored if ($ore > $ore_stored);
+    foreach my $type (ORE_TYPES) {
+        my $stored = $body->type_stored($type);
+        if ($stored) {
+          my $amt = int(($ore * $stored)/$ore_stored) - 100;
+          if ( $amt > 0 ) {
+            $body->spend_type($type, $amt);
+            $payload->{resources}{$type} = $amt;
+          }
+        }
+    }
+    $self->payload($payload);
+    $self->update;
+    $body->update;
 };
 
 1;
