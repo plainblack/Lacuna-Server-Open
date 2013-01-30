b0d3c5d90ecb2f1174c1edea1322665be91b13f0
Mark Lemming <lemming@quirkyqatz.com>
2013-01-30 10:09:50 -0700

    Incoming chains recalculated as well.

--- lib/Lacuna/RPC/Building/BlackHoleGenerator.pm
+++ lib/Lacuna/RPC/Building/BlackHoleGenerator.pm
@@ -816,6 +816,7 @@ sub bhg_move_system {
 # We need to redo all the chains of the moved planets in one go.
         if ($bod->empire and $bod->get_type ne 'asteroid') {
             $bod->recalc_chains;
+            recalc_incoming_supply($bod);
         }
     }
     return {
@@ -932,6 +933,7 @@ sub bhg_swap {
                 }
             }
             $target->recalc_chains; # Recalc all chains
+            recalc_incoming_supply($target);
             if ($new_data->{type} eq 'space station') {
                 drop_stars_beyond_range($target);
             }
@@ -976,6 +978,7 @@ sub bhg_swap {
             }
         }
         $body->recalc_chains; # Recalc all chains
+        recalc_incoming_supply($body);
         if ($body->get_type eq 'space station') {
             drop_stars_beyond_range($target);
         }
@@ -1028,6 +1031,25 @@ sub bhg_swap {
         swapname => $new_data->{name},
         swapid   => $new_data->{id},
     };
+}
+
+sub recalc_incoming_supply {
+    my ($body) = @_;
+
+    my $all_chains = $body->in_supply_chains;
+
+    my %bids;
+    while (my $chain = $all_chains->next) {
+        my $bid = $chain->planet_id;
+        next if defined($bids{$bid});
+        $bids{$bid} = 1;
+        my $sender = Lacuna->db
+            ->resultset('Lacuna::DB::Result::Map::Body')
+            ->find($bid);
+        if (defined($sender->empire)) {
+            $sender->recalc_chains; # Recalc all chains
+        }
+    }
 }
 
 sub drop_stars_beyond_range {
