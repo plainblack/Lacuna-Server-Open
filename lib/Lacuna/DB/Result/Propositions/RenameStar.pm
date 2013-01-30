9e678d1eadad98924c052d72576990fd52c828ad
Mark Lemming <lemming@quirkyqatz.com>
2013-01-30 12:42:06 -0700

    Fixed case of star being dropped from jurisdiction, but rename prop exists.

--- lib/Lacuna/DB/Result/Propositions/RenameStar.pm
+++ lib/Lacuna/DB/Result/Propositions/RenameStar.pm
@@ -8,13 +8,13 @@ extends 'Lacuna::DB::Result::Propositions';
 before pass => sub {
     my ($self) = @_;
     my $station = $self->station;
-    my $star = $station->stars->find($self->scratch->{star_id});
+    my $star = Lacuna->db->resultset('Lacuna::DB::Result::Map::Star')->find($self->scratch->{star_id});
     my $name = $self->scratch->{name};
-    if (Lacuna->db->resultset('Lacuna::DB::Result::Map::Star')->search({name=>$name, 'id'=>{'!='=>$star->id}})->count) {
-        $self->pass_extra_message('Unfortunately, by the time the proposition passed, the name *'.$name.'* had already been taken, effectively nullifying the vote.');
-    }
-    elsif ($star->station_id != $station->id) {
+    if (!defined($star) or $star->station_id != $station->id) {
         $self->pass_extra_message('Unfortunately, by the time the proposition passed, the star was no longer under the jurisdiction of this station, effectively nullifying the vote.');
+    }
+    elsif (Lacuna->db->resultset('Lacuna::DB::Result::Map::Star')->search({name=>$name, 'id'=>{'!='=>$star->id}})->count) {
+        $self->pass_extra_message('Unfortunately, by the time the proposition passed, the name *'.$name.'* had already been taken, effectively nullifying the vote.');
     }
     else {
         $star->name($name);
