f4e7ed34aeb46000c23c8223b88377016e5b6e6f
Iain C Docherty <github@iandocherty.com>
2013-03-22 18:51:32 -0400

    Romoved the Halls of Vrbansk

--- lib/Lacuna/DB/Result/Building/Permanent/HallsOfVrbansk.pm
+++ lib/Lacuna/DB/Result/Building/Permanent/HallsOfVrbansk.pm
@@ -11,9 +11,22 @@ with "Lacuna::Role::Building::CantBuildWithoutPlan";
 
 use constant controller_class => 'Lacuna::RPC::Building::HallsOfVrbansk';
 
-sub can_upgrade {
-    confess [1013, "You can't upgrade the Halls of Vrbansk."];
-}
+around can_build => sub {
+    confess [1013,"You can't build the Halls of Vrbansk."];
+};
+
+around can_upgrade => sub {
+    confess [1013,"You can't upgrade the Halls of Vrbansk."];
+};
+
+around can_downgrade => sub {
+    confess [1013,"You can't downgrade the Halls of Vrbansk."];
+};
+
+around can_demolish => sub {
+    confess [1013,"You can't demolish the Halls of Vrbansk."];
+};
+
 
 use constant image => 'hallsofvrbansk';
 
@@ -22,43 +35,8 @@ sub image_level {
     return $self->image.'1';
 }
 
-after finish_upgrade => sub {
-    my $self = shift;
-    $self->body->add_news(30, sprintf('The ancient wisdom of the Great Race is still alive on %s.', $self->body->name));
-};
-
-sub get_halls {
-    my $self = shift;
-    my @halls = grep {$_->is_upgrading == 0} $self->body->get_buildings_of_class('Lacuna::DB::Result::Building::Permanent::HallsOfVrbansk');
-    return @halls;
-}
-
-sub get_upgradable_buildings {
-    my ($self) = @_;
-    my $body    = $self->body;
-    $body->update;
-    # The max_level is represented by the number of halls already
-    # built, plus the number of hall plans
-    my $halls = $self->get_halls;
-    my ($plan) = grep {$_->class eq 'Lacuna::DB::Result::Building::Permanent::HallsOfVrbansk'} @{$body->plan_cache};
-    my $plans = defined $plan ? $plan->quantity : 0;
-
-    my $max_level = $halls + $plans;
-    $max_level = 30 if $max_level > 30;
-
-    my @buildings = grep {
-        ($_->level  < $max_level) and
-        ($_->class  =~ /Permanent/) and
-        ($_->class  ne 'Lacuna::DB::Result::Building::Permanent::TheDillonForge') and
-        ($_->class  ne 'Lacuna::DB::Result::Building::Permanent::HallsOfVrbansk') and
-        ($_->is_upgrading == 0)
-    } @{$self->body->building_cache};
-    return \@buildings;
-}
-
 use constant name => 'Halls of Vrbansk';
 use constant time_to_build => 0;
-
 
 no Moose;
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
