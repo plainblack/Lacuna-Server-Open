8b5cbc903a71bc66baa6594853894e66871e73c5
Iain C Docherty <github@iandocherty.com>
2013-03-23 12:38:08 -0400

    Fixed regression problem with 'building forms naturally'.

--- lib/Lacuna/Role/Building/FormsNaturally.pm
+++ lib/Lacuna/Role/Building/FormsNaturally.pm
@@ -4,8 +4,8 @@ use Moose::Role;
 
 around can_build => sub {
     my ($orig, $self, $body) = @_;
-    if ($body->get_plan(__PACKAGE__, 1)) {
-        return $orig->($self, $body);
+    if ($body->get_plan(ref $self, 1)) {
+        return $self->$orig($body);
     }
     confess [1013,"You can't build ".$self->name.". It forms naturally."];
 };
