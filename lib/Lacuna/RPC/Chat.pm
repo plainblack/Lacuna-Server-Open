630ebdd3d228e1f601d04ef81792ce2ebd36164a
Iain C Docherty <git@iain-docherty.com>
2014-08-25 20:40:20 +0000

    Corrected session_id

--- lib/Lacuna/RPC/Chat.pm
+++ lib/Lacuna/RPC/Chat.pm
@@ -9,7 +9,7 @@ use Firebase;
 use Gravatar::URL;
 
 sub init_chat {
-    my ($self, $session) = @_;
+    my ($self, $session_id) = @_;
 
     my $empire = $self->get_empire_by_session($session_id);
 
