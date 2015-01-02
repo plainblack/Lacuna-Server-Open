8a18b21ea40a260a1105a207701579a58544d445
Darin McBride <dmcbride@cpan.org>
2015-01-01 22:33:15 -0700

    Order incoming ships list at the db so that everything in page 1 comes before everything in page 2.

--- lib/Lacuna/RPC/Building/SpacePort.pm
+++ lib/Lacuna/RPC/Building/SpacePort.pm
@@ -984,7 +984,7 @@ sub view_foreign_ships {
     $page_number ||= 1;
     my @fleet;
     my $now = time;
-    my $ships = $building->foreign_ships->search({}, {rows=>25, page=>$page_number, join => 'body' });
+    my $ships = $building->foreign_ships->search({}, {rows=>25, page=>$page_number, join => 'body', order_by => 'date_available'});
     my $see_ship_type = ($building->effective_level * 350) * ( $building->effective_efficiency / 100 );
     my $see_ship_path = ($building->effective_level * 450) * ( $building->effective_efficiency / 100 );
     my @my_planets = $empire->planets->get_column('id')->all;
