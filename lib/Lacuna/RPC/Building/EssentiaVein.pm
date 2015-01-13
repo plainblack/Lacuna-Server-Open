206efb659d63b348a4edde4670345093e6751012
Darin McBride <dmcbride@cpan.org>
2015-01-13 13:43:24 -0700

    Add draining of essentia veins

--- lib/Lacuna/RPC/Building/EssentiaVein.pm
+++ lib/Lacuna/RPC/Building/EssentiaVein.pm
@@ -13,6 +13,44 @@ sub model_class {
     return 'Lacuna::DB::Result::Building::Permanent::EssentiaVein';
 }
 
+around view => sub {
+    my ($orig, $self, $session_id, $building_id) = @_;
+    my $empire = $self->get_empire_by_session($session_id);
+    my $building = $self->get_building($empire, $building_id);
+    my $out = $orig->($self, $empire, $building);
+
+    $out->{building}{drain_capable} = int($building->work_seconds_remaining() / (30 * 24 * 60 * 60));
+
+    return $out;
+};
+
+sub drain {
+    my ($self, $session_id, $building_id, $times) = @_;
+    my $empire = $self->get_empire_by_session($session_id);
+    my $building = $self->get_building($empire, $building_id);
+
+    $times ||= 1;
+    my $days = $times * 30;
+
+    my $work_reduction = $days * 24 * 60 * 60;
+    confess [1010, "The essentia vein does not have $days days left on it."]
+        unless $building->work_ends > DateTime->now->add(seconds => $work_reduction);
+
+    $empire->add_essentia({ 
+                    amount  => $days,
+                    reason  => "Essentia Vein (drain: $days)",
+                });
+    $empire->update;
+
+    my $work_ends = $building->work_ends->subtract(seconds => $work_reduction);
+    $building->reschedule_work($work_ends);
+    $building->update;
+
+    return $self->view($session_id, $building_id);
+}
+
+__PACKAGE__->register_rpc_method_names(qw(drain));
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 
