9e550443b11c76200f375e0e8ac5f38d09ba93f9
Darin McBride <dmcbride@cpan.org>
2015-01-14 12:18:14 -0700

    PCC should ignore uni level for its purposes (so that new players can get full benefit from their PCC while starting the game).

--- lib/Lacuna/DB/Result/Building/PlanetaryCommand.pm
+++ lib/Lacuna/DB/Result/Building/PlanetaryCommand.pm
@@ -4,6 +4,7 @@ use Moose;
 use utf8;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Building';
+with 'Lacuna::Role::Building::IgnoresUniversityLevel';
 
 around 'build_tags' => sub {
     my ($orig, $class) = @_;
