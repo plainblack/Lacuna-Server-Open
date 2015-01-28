7440a544cdd1a947d6d4c463fa069a16218f0664
Darin McBride <dmcbride@cpan.org>
2015-01-28 11:04:36 -0700

    Index creation during upgrade can be skipped since it is already done manually. New expanses will still get the index via News.pm in 09c345e94

--- var/upgrades/3.0909.sql
+++ var/upgrades/3.0909.sql
@@ -2,4 +2,3 @@ alter table ships add column number_of_docks int default 1;
 alter table ships modify hold_size bigint;
 alter table battle_log add column attacking_number int default 1;
 alter table battle_log add column defending_number int default 1;
-alter table news add index idx_date_posted (date_posted);
