2e3ec9d58f77acd52c16d59bd38179ee3a1a3ca9
Darin McBride <dmcbride@cpan.org>
2015-01-28 11:04:17 -0700

    Sitters can still chat for AIs

--- lib/Lacuna/RPC/Chat.pm
+++ lib/Lacuna/RPC/Chat.pm
@@ -17,7 +17,7 @@ sub init_chat {
     my $config = Lacuna->config;
     my $firebase_config = $config->get('firebase');
     return undef unless $firebase_config;
-    return undef if $empire->current_session->is_sitter; 
+    return undef if $empire->current_session->is_sitter && $empire->id > 1;
     my $chat_auth = Firebase::Auth->new(
         secret  => $firebase_config->{auth}{secret},
 #        debug   => \1,
