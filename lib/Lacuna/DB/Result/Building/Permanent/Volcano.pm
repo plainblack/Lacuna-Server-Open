c80c6d203a41fc7dc4440dfb2158a8c71de2a930
Imzogelmo <Imzogelmo@gmail.com>
2015-03-25 12:13:39 -0500

    Update Volcano.pm
    Numbers make headlines more shocking

--- lib/Lacuna/DB/Result/Building/Permanent/Volcano.pm
+++ lib/Lacuna/DB/Result/Building/Permanent/Volcano.pm
@@ -4,6 +4,7 @@ use Moose;
 use utf8;
 no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Building::Permanent';
+use Lacuna::Util qw(randint);
 
 with "Lacuna::Role::Building::UpgradeWithHalls";
 with "Lacuna::Role::Building::CantBuildWithoutPlan";
@@ -19,10 +20,10 @@ sub image_level {
 
 after finish_upgrade => sub {
     my $self = shift;
-    $self->body->add_news(30, 'A volcano erupted on %s today spewing tons of ash on the inhabitants.', $self->body->name);
+    $self->body->add_news(30, 'A volcano erupted on %s today spewing %s million tons of ash on the inhabitants.', $self->body->name, randint(50,250) );
 };
 
-use constant name => 'Volcano';
+use constant name => 'Volcano'; 
 use constant ore_production => 4000;
 
 use constant time_to_build => 0;
