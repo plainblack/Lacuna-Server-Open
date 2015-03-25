65ff8c5a90a906917dd783b79c72b0142e442076
Imzogelmo <Imzogelmo@gmail.com>
2015-03-25 11:36:39 -0500

    Update DeploySupplyPod.pm
    indents cleaned up

--- lib/Lacuna/Role/Ship/Arrive/DeploySupplyPod.pm
+++ lib/Lacuna/Role/Ship/Arrive/DeploySupplyPod.pm
@@ -17,14 +17,13 @@ after handle_arrival_procedures => sub {
     # deploy the pod
     my $body = $self->foreign_body;
     my ($x, $y) = eval{$body->find_free_space};
-    if ($@)	{   
-	    
-    # notify home of lost ship 
-    $self->body->empire->send_predefined_message(
-         tags        => ['Alert'],
-         filename    => 'no_space_for_ship.txt',
-         params      => [$self->type, $body->x, $body->y, $body->name, " a mid-air collision"],
-     );
+    if ($@) {   
+        # notify home of lost ship 
+         $self->body->empire->send_predefined_message(
+             tags        => ['Alert'],
+             filename    => 'no_space_for_ship.txt',
+             params      => [$self->type, $body->x, $body->y, $body->name, " a mid-air collision"],
+         );
     
     $body->add_news(10 ,"Humanitarian mission bound for %s lost during final entry", $body->name);
     
