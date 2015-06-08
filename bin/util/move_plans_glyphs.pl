1a22ac552f3b1f814a740358105a973c10093cd6
Darin McBride <dmcbride@cpan.org>
2015-06-08 10:52:29 -0600

    Simplify, use Lacuna::DB's auto-where handler to allow both body IDs and names as options so that user doesn't need to look up body IDs all the time. (Will eventually transplant this to other scripts as well.)

--- bin/util/move_plans_glyphs.pl
+++ bin/util/move_plans_glyphs.pl
@@ -1,34 +1,27 @@
 use 5.010;
 use strict;
 use lib '/data/Lacuna-Server/lib';
-use Lacuna::DB;
-use Lacuna;
-use Lacuna::Util qw(format_date);
+use L;
 use Getopt::Long;
-use JSON;
-use SOAP::Amazon::S3;
-use Lacuna::Constants qw(SHIP_TYPES ORE_TYPES);
 use utf8;
 
 
   $|=1;
 
-  our $quiet;
   our $from;
   our $to;
   our $move_p;
   our $move_g;
 
   GetOptions(
-    'quiet'   => \$quiet,  
-    'from=i'  => \$from,
-    'to=i'    => \$to,
-    'plans'   => \$move_p,
-    'glyphs'  => \$move_g,
+    'quiet|q'   => \$quiet,  
+    'from|f=s'  => \$from,
+    'to|t=s'    => \$to,
+    'plans|p'   => \$move_p,
+    'glyphs|g'  => \$move_g,
   );
 
   die "Must give from and to body ids\n" unless ($from and $to);
-  die "to and from must be different!\n" if ($from == $to);
   die "Must give either define plans or glyphs to move\n" unless ($move_g or $move_p);
   out('Started');
   my $start = DateTime->now;
@@ -36,8 +29,9 @@ use utf8;
   out('Loading DB');
   our $db = Lacuna->db;
 
-  my $body_from = $db->resultset('Map::Body')->find($from);
-  my $body_dest = $db->resultset('Map::Body')->find($to);
+  my $body_from = $db->body($from);
+  my $body_dest = $db->body($to);
+  die "to and from must be different!\n" if ($body_from->id == $body_dest->id);
 
   out(sprintf("%30s -> %30s", $body_from->name, $body_dest->name));
 
@@ -67,9 +61,3 @@ use utf8;
   out(sprintf("Moved %3d/%5d plan t/q; Moved %3d/%5d glyph t/q", $ptypes, $pquant, $gtypes, $gquant));
 exit;
 
-sub out {
-    my $message = shift;
-    unless ($quiet) {
-        say format_date(DateTime->now), " ", $message;
-    }
-}
