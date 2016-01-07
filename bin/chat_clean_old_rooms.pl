68d35ec63129224c4ed61e6dafc2caad95658ded
lemming552 <lemming@quirkyqatz.com>
2016-01-07 14:30:35 -0700

    Colapse output of chat clean

--- bin/chat_clean_old_rooms.pl
+++ bin/chat_clean_old_rooms.pl
@@ -30,18 +30,34 @@ say "Fetching users attached to rooms.";
 my $room_users = $firebase->get('room-users');
 
 say "Deleting abandoned rooms.";
+my %room_clear;
+my %room_keep;
 foreach my $room (keys %{$rooms}) {
     if ($rooms->{$room}{type} eq 'official') {
         say "Skipping ".$rooms->{$room}{name}.", because it's an official room.";
     }
     elsif (exists $room_users->{$room}) {
-        say "Skipping ".$rooms->{$room}{name}.", because it has connected users.";
+        if ($room_keep{"$rooms->{$room}{name}"}) {
+            $room_keep{"$rooms->{$room}{name}"} += 1;
+        }
+        else {
+            $room_keep{"$rooms->{$room}{name}"} = 1;
+        }
     }
     else {
-        say "Deleting ".$rooms->{$room}{name}.".";
+        if ($room_clear{"$rooms->{$room}{name}"}) {
+            $room_clear{"$rooms->{$room}{name}"} += 1;
+        }
+        else {
+            $room_clear{"$rooms->{$room}{name}"} = 1;
+        }
         $firebase->delete('room-messages/'.$room);
         $firebase->delete('room-metadata/'.$room);
     }
 }
-
-
+foreach my $key (sort keys %room_clear) {
+    say "Deleted ".$key." ".$room_clear{"$key"}.".";
+}
+foreach my $key (sort keys %room_keep) {
+    say "Skipping ".$key." ".$room_keep{"$key"}.".";
+}
