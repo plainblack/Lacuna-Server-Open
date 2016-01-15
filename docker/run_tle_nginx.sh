72ed17d38a9ee52a71a6eb4116f6b51d5e7f9e57
Iain C Docherty <iain@iain-docherty.com>
2016-01-15 15:14:21 -0500

    Added some documentation

--- docker/run_tle_nginx.sh
+++ docker/run_tle_nginx.sh
@@ -1,1 +1,1 @@
-docker run -it -p ${TLE_NETWORK_LISTEN:-127.0.0.1}:${TLE_NETWORK_PORT:-8080}:80 --name=tle-nginx -v ${PWD}/../etc/nginx.conf:/etc/nginx/nginx.conf:ro -v ${PWD}/../captcha:/data/Captcha -v ${PWD}/../etc:/data/Lacuna-Server/etc -v ${PWD}/../var:/data/Lacuna-Server/var -d icydee/tle-nginx
+docker run -it -p ${TLE_SERVER_LISTEN:-127.0.0.1}:${TLE_SERVER_PORT:-8000}:80 --name=tle-nginx -v ${PWD}/../etc/nginx.conf:/etc/nginx/nginx.conf:ro -v ${PWD}/../captcha:/data/captcha -v ${PWD}/../etc:/data/Lacuna-Server/etc -v ${PWD}/../var:/data/Lacuna-Server/var -d icydee/tle-nginx
