f2cebf87eeb074398fb2c1234d5c4ee276830881
Iain C Docherty <iain@iain-docherty.com>
2016-03-14 13:49:25 -0400

    Added to Credits

--- lib/Lacuna/RPC/Stats.pm
+++ lib/Lacuna/RPC/Stats.pm
@@ -9,7 +9,8 @@ use Lacuna::Constants qw(SHIP_TYPES);
 sub credits {
     return [
             { 'Game Design'         => ['JT Smith','Jamie Vrbsky']},
-            { 'Web Client'          => ['John Rozeske','Graham Knop','Matthew Musgrove','vaelxon (Spy Training GUI)', 'Nathan McCallum (1vasari)']},
+            { 'Web Client'          => ['John Rozeske','Graham Knop','Matthew Musgrove','vaelxon (Spy Training GUI)']},
+            { 'Web Client redesign' => ['Nathan McCallum (1vasari)', 'Iain C Docherty (icydee)']},
             { 'iPhone Client'       => ['Kevin Runde','RedOrion']},
             { 'Game Server'         => ['JT Smith','Graham Knop','Matthew Musgrove','Iain C Docherty (icydee)', 'Mark Lemming (Norway)', '(fireartist)', '(Winton-Akagane)', '(Ysthane)', 'Darin McBride (The Tower)', 'RedOrion' ]},
             { 'Art and Icons'       => ['Ryan Knope','JT Smith','Joseph Wain / glyphish.com','Keegan Runde']},
@@ -18,6 +19,7 @@ sub credits {
             { 'Game Support'        => ['Plain Black Corporation / plainblack.com','Mary Hoerr', 'Daniel Collins (United Federation)', 'RedOrion', 'Infinate Ones']},
             { 'Lost City of Tyleon' => ['Steven Binns','Owen Ferguson','Garloo','Mark Lemming (Norway)','Saint','Shadow','JT Smith','Noel Sorensen','James T']},
             { 'AI Design'           => ['JT Smith','Iain C Docherty (icydee)', 'Mark Lemming (Norway)']},
+            { 'Reboot design'       => ['Iain C Docherty (icydee)', 'Mark Lemming (Norway)']},
             ];
 }
 
