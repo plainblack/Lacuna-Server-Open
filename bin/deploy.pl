fda944fbd7f9685fb900e10c537130d4280b8887
JT Smith <jt@lacunaexpanse.com>
2016-03-30 16:00:01 -0400

    Updated deploy.pl script

--- bin/deploy.pl
+++ bin/deploy.pl
@@ -159,14 +159,14 @@ END_TEXT
             my $restart_server = 0;
             # Reboot code
             if ($branch eq "pt-reboot") {
-                chdir('/data/Lacuna-Server'');
+                chdir('/data/Lacuna-Server');
                 system("git", "pull", "origin", "pt-reboot");
                 $restart_server = 1;
             }
 
             # Legacy code
             elsif ($branch eq "pt") {
-                chdir('/data/Lacuna-Server-Open'');
+                chdir('/data/Lacuna-Server-Open');
                 system("git", "pull", "origin", "pt");
                 $restart_server = 1;
             }
